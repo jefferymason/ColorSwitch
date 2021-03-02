5840682c43d369d33e0fad0cd55da8637b54f4f8
Jeffery Wayne Mason <jefferymason72@gmail.com>
2021-03-02 10:31:10 -0600

    done

---
+++ ColorSwitch/Template/Settings.swift
@@ -0,0 +1,14 @@
+//
+//  Settings.swift
+//  ColorSwitch
+//
+//  Created by Jeffery Mason on 3/2/21.
+//
+
+import SpriteKit
+
+enum PhysicsCategories {
+    static let none: UInt32 = 0
+    static let ballCategory: UInt32 = 0x1
+    static let switchCategory: UInt32 = 0x1 << 1
+}
